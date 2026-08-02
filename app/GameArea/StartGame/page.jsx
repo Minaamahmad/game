e4bf9927e2eb6dc781c8545b29e37da88865225d
minaamahmad@gmail.com <minaamahmad@gmail.com>
2026-08-02 21:39:15 +0500

    feat: refactor Startgame component to include socket functionality and player turn logic

--- app/GameArea/StartGame/page.jsx
+++ app/GameArea/StartGame/page.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
+ import { useSocket } from "@/Context/contextapi.js";
 
-const page = () => {
+const Startgame = () => {
+  const socket=useSocket()
+  const [turn,setTurn]=useState("")
+
+const playerTurn=()=>{
+  if(!socket || !turn.trim()) return;
+
+  socket.emit("turn",turn,(res)=>{
+if (res.success) {
+        setError("")
+        setTurn("")
+      } else {
+        setError(res.message || "Invalid move")
+      }
+  })
+}
   return (
     <div>
     <h1> Players have joined the room starting gamem</h1>
+
+    <input type="text" value={turn}  onChange={(e)=>setTurn(e.target.value)}/>
+<button onClick={playerTurn}></button>
+    <p></p>
     </div>
   )
 }
 
-export default page
+export default Startgame
